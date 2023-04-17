041c43a218f02d590e6306ff7c8424d34ccd500b
Sergey <abcdefg1214489@gmail.com>
2023-04-17 20:40:40 +0600

    Add unended API call for Digiseller

--- backend/index.php
+++ backend/index.php
@@ -1,1 +1,27 @@
 <?php
+
+error_reporting(E_ALL);
+
+$url = "https://api.digiseller.ru/api/apilogin";
+$id = "C256592A10714D69BFB237649D2C8110";
+$seller_id = '1097528';
+
+$timestamp = time();
+$hash_data = $id . $timestamp;
+$sign = hash('sha256', $hash_data);
+
+$send_data = array('seller_id' => $seller_id, 'sign' => $sign, 'timestamp' => $timestamp);
+$headers = array('Content-Type' => 'application/json');
+
+$ch = curl_init('https://api.digiseller.ru/api/apilogin');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($send_data));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($ch);
+curl_close($ch);
+
+$response = json_decode($result, true);
+
+echo "JSON Response: \n";
+echo json_decode($response, JSON_PRETTY_PRINT);
