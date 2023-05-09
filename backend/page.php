da14000b298def4994225048a0acceffb65fff64
SERG <abcdefg1214489@gmail.com>
2023-05-09 23:35:33 +0600

    Fix the last commit

--- backend/page.php
+++ backend/page.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <a href="#openModal">Открыть модальное окно</a>
@@ -32,8 +32,8 @@ require_once __DIR__ . "/vendor/autoload.php";
 require_once __DIR__ . '/configuration.php';
 require_once __DIR__ . '/functions.php';
 
-$body = "Hello world";
-var_dump(send_mail($mail_settings, ['abcdefg1214489@gmail.com'], 'Hello world', $body ));
+$body = "Здарова";
+var_dump(send_mail($mail_settings, ['abcdefg1214489@gmail.com'], 'Test-letter', $body ));
 
 ?>
 
