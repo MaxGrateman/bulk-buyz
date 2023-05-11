9994ae67f3d80e0103482465bac98030e1a59eae
Max Astakhov <grekf2@mail.ru>
2023-05-12 00:20:17 +0600

    Add reviews section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route, Link} from "react-router-dom";
 import Header from './components/application/header/Header'
 import Cards from './components/application/cards/Cards'
+import Reviews from "./components/application/reviews/Reviews";
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="wrapper">
         <Header/>
         <Cards/>
+        <Reviews/>
     </div>
   )
 }
---
+++ frontend/src/components/application/reviews/Reviews.tsx
@@ -0,0 +1,34 @@
+import './Reviews.css'
+import CommentList from "../../interfaces/CommentList/CommentList";
+import {IComment} from "../../types/types";
+
+function Reviews() {
+
+    const comments: IComment[] = [
+        {id: 1, data: '1 мая, 2023', text: 'Спасибо. Всё отлично! Как и заявленно в описании'},
+        {id: 2, data: '26 мая, 2023', text: 'Good'},
+        {id: 3, data: '2 августа, 2023', text: 'Всё прекрасно!'},
+    ]
+
+
+    return(
+        <div className="reviews">
+            <div className="reviews_wrapper">
+                <label className="reviews_label">Отзывы</label>
+                <div className="reviews_under_label">Отзывы о нас</div>
+                <div className='reviews_columns'>
+                    <div className="reviews_column">
+                        <a className="reviews_column_links">Отзывы на площадке Plati.market</a>
+                        <CommentList comments={comments}/>
+                    </div>
+                    <div className="reviews_column">
+                        <a className="reviews_column_links">Отзывы на площадке GGSEL</a>
+                        <CommentList comments={comments}/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Reviews
