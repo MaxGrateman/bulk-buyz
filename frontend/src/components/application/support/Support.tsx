5bfc8405651c49e58e4d80899c7a346ad7414cea
Max Astakhov <grekf2@mail.ru>
2023-06-04 01:57:49 +0600

    Fix accordion

--- frontend/src/components/application/support/Support.tsx
+++ frontend/src/components/application/support/Support.tsx
@@ -38,35 +38,7 @@ import Accordion from "../../interfaces/Accordion/Accordion";
                 <label className="support_label">FAQ</label>
                 <p className="support_underlabel">Часто заводаемые вопросы</p>
                 <div>
-                    {/*<button className="accordion">Что мне будет доступно после смены региона?</button>
-                    <div className="support_accordion">
-                        <p className="support_bar_text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-                    </div>*/}
-
-                        <Accordion data={accordionData}/>
-
-
-                   {/* <button className="support_accordion">Какие данные я должен предоставить для смены региона?</button>
-                    <div className="support_bar">
-                        <p className="support_bar_text">Какие данные я должен предоставить для смены региона?</p>
-                    </div>
-
-                    <button className="support_accordion">Могу ли я вам доверять?</button>
-                    <div className="support_bar">
-                        <p className="support_bar_text">Могу ли я вам доверять?</p>
-                    </div>
-
-                    <button className="support_accordion">Что делать если Макс не хочет работать?</button>
-                    <div className="support_bar">
-                        <p className="support_bar_text">Что делать если Макс не хочет работать?</p>
-                    </div>
-
-                    <button className="support_accordion">Как заставить Макса работать?</button>
-                    <div className="support_bar">
-                        <p className="support_bar_text">Как заставить Макса работать?</p>
-                    </div>*/}
-
+                    <Accordion data={accordionData}/>
                 </div>
             </div>
         </div>
--- frontend/src/components/interfaces/Accordion/Accordion.tsx
+++ frontend/src/components/interfaces/Accordion/Accordion.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Accordion.css'
+import plus from '../../../assets/cross.svg'
 
 interface AccordionData {
     title: string;
@@ -11,27 +12,33 @@ interface AccordionProps {
 }
 
 const Accordion: React.FC<AccordionProps> = ({ data }) => {
-    const [activeIndex, setActiveIndex] = useState<number | null>(null);
+    const [activeIndex, setActiveIndex] = useState<boolean[]>(new Array(data.length).fill(false));
 
     const handleClick = (index: number) => {
-        setActiveIndex((prevIndex) => (prevIndex === index ? null : index));
+        setActiveIndex((prevActiveIndex) => {
+            const newState = [...prevActiveIndex];
+            newState[index] = !newState[index]; // Инвертируем состояние аккордеона
+            return newState;
+        });
     };
 
     return (
         <div>
             {data.map((accordion, index) => (
                 <div
-                    className={`accordion ${activeIndex === index ? 'active' : ''}`}
+                    className={`accordion ${activeIndex[index] ? 'active' : ''}`}
                     onClick={() => handleClick(index)}
                     key={index}
                 >
-                    <div className={`title ${activeIndex === index ? 'activeTitle' : ''}`}>
+                    <div className={`title ${activeIndex[index] ? 'activeTitle' : ''}`}>
                         {accordion.title}
-                        <span className={`plus ${activeIndex === index ? 'activePlus' : '+'}`}>
-                              {activeIndex === index ? '+' : '+'}
-                        </span>
+                        <img
+                            src={plus}
+                            alt="plus-icon"
+                            className={`plus ${activeIndex[index] ? 'activePlus' : ''}`}
+                        />
                     </div>
-                    {activeIndex === index && <div className="panel" >{accordion.content}</div>}
+                    {activeIndex[index] && <div className="panel">{accordion.content}</div>}
                 </div>
             ))}
         </div>
