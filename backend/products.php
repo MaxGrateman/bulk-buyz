70bcfb2383a5d53bc0ce9e87726bfad2456aacfb
Max Astakhov <grekf2@mail.ru>
2023-07-07 22:05:36 +0600

    Committing changes to backend/products.php before merge

--- backend/products.php
+++ backend/products.php
@@ -3,6 +3,7 @@ header("Access-Control-Allow-Headers: application/json");
 header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json");
 
+
 require_once __DIR__ . '/credentials.php';
 require_once __DIR__ . '/DatabaseConnection.php';
 
