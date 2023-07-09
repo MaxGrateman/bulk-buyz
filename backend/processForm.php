8198411db93f034aa6388e2bd0230a542e8b74cb
SERG <abcdefg1214489@gmail.com>
2023-07-10 01:33:28 +0600

    Add header (test)

--- backend/processForm.php
+++ backend/processForm.php
@@ -1,6 +1,7 @@
 <?php
-header("Access-Control-Allow-Headers: application/json");
 header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Headers: Content-Type");
+
 
 require_once __DIR__ . '/credentials.php';
 require_once __DIR__ . '/DatabaseConnection.php';
@@ -11,21 +12,21 @@ $connection = $database->getConnection();
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
-    $selectedProduct = $_POST['product'];
+echo $email; }
 
-    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $query = "INSERT INTO users (email, name, variant, price, time) VALUES (:email, 'Test', :selectedProduct, '228', NOW())";
-        $stmt = $database->prepare($query);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':selectedProduct', $selectedProduct);
-        $stmt->execute();
-        $count = $stmt->rowCount();
-        if ($count > 0) {
-            echo "E-mail записан в БД";
-        } else {
-            echo "Произошла ошибка при записи в БД";
-        }
-    } else {
-        echo 'Некорректный адрес электронной почты';
-    }
-}
+//  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+//        $query = "INSERT INTO users (email, name, variant, price, time) VALUES (:email, 'Test', :selectedProduct, '228', NOW())";
+//        $stmt = $database->prepare($query);
+//        $stmt->bindParam(':email', $email);
+//        $stmt->bindParam(':selectedProduct', $selectedProduct);
+//        $stmt->execute();
+//        $count = $stmt->rowCount();
+//        if ($count > 0) {
+//            echo "E-mail записан в БД";
+//        } else {
+//            echo "Произошла ошибка при записи в БД";
+//        }
+//    } else {
+//        echo 'Некорректный адрес электронной почты';
+//    }
+//}
