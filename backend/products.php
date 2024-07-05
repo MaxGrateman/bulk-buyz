0151c01ed22001e04a7a15bf08d8150fa3c94a3e
Max Astakhov <grekf2@mail.ru>
2024-07-05 20:16:28 +0500

    Change php headers

--- backend/products.php
+++ backend/products.php
@@ -1,7 +1,9 @@
 <?php
 header("Access-Control-Allow-Headers: application/json");
-header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: https://bulk-buyz.netlify.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 require_once __DIR__ . '/credentials.php';
 require_once __DIR__ . '/DatabaseConnection.php';
